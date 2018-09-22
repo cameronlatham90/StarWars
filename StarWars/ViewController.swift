b45f36834aa4e11373927567a3a632ce1b98ca1f
Cameron Latham <cameronlatham@Camerons-MacBook-Pro.local>
2018-09-22 12:06:57 -0500

    initial commit

---
+++ StarWars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWars
+//
+//  Created by Cameron Latham on 9/22/18.
+//  Copyright © 2018 Cameron Latham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
